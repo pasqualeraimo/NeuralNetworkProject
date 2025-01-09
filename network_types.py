9078f1e5e24d3ddbc9ef8d5443085f2a94df660f
Pasquale <pasquale.raimo270@gmail.com>
2025-01-09 15:05:16 +0100

    Define type aliases for activation and error functions
    Added `ActivationFunction` and `ErrorFunction` type hints for improved code readability and type safety. These aliases specify the expected input and output formats for activation and error functions in neural network implementations.

---
+++ network_types.py
@@ -0,0 +1,15 @@
+import numpy as np
+from typing import Callable
+
+# Tipo per la funzione di attivazione, che accetta:
+# - Un array numpy.ndarray, che rappresenta l'input alla funzione.
+# - Un booleano, che indica se si desidera calcolare la derivata della funzione.
+# Restituisce un array numpy.ndarray che rappresenta l'output elaborato dalla funzione di attivazione o la sua derivata.
+ActivationFunction = Callable[[np.ndarray, bool], np.ndarray]
+
+# Tipo per la funzione di errore, che accetta:
+# - Un array numpy.ndarray, che rappresenta il valore predetto dalla rete neurale.
+# - Un array numpy.ndarray, che rappresenta il valore atteso.
+# - Un booleano, che indica se si desidera calcolare la derivata della funzione di errore.
+# Restituisce un array numpy.ndarray che rappresenta l'errore calcolato o la sua derivata.
+ErrorFunction = Callable[[np.ndarray, np.ndarray, bool], np.ndarray]
